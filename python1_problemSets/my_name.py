fa921a6bcdd6edcf9e80db727aea3f68993a0c8a
student <student@info-25.cshl.edu>
2022-10-11 11:01:01 -0400

    remove old files

---
+++ python1_problemSets/my_name.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+print('My name: ', name, '\nMy favorite color: ', color, '\nMy favorite activity: ', activity, '\nMy favorite animal: ', animal)
