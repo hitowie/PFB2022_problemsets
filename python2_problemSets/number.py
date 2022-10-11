c34a050acf960e6ada7fde5bc515960c4a41205e
student <student@info-25.cshl.edu>
2022-10-11 16:05:22 -0400

    fixed script number.py for any number

--- python2_problemSets/number.py
+++ python2_problemSets/number.py
@@ -4,12 +4,20 @@ import sys
 x = int(sys.argv[1])
 
 if x > 0:
-	message = 'positive'
-	print(message)
+	if x < 50:
+		if x % 2 == 0:
+			message = 'it is an even number that is smaller than 50'
+		else:
+			message = 'it is an uneven number that is smaller than 50'
+	elif x>50:
+		if x % 3 == 0:
+			message = 'it is larger than 50 and divisible by 3'
+		else:
+			message = 'it is larger than 50 and cannot be devided by 3'
+	else:
+		message = 'number is 50'
 elif x < 0:
 	message = 'negative'
-	print(message)
 else:
 	message = 'zero'
-	print(message)
-
+print(message)
