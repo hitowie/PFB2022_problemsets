7b7607414bb19c76e593ab72be94860f70739b06
student <student@info-25.cshl.edu>
2022-10-11 10:41:43 -0400

    added name, color, activity, animal to print

--- my_name.py
+++ my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import sys
-name = sys.argv[1]
-print('My name: ', name) 
 
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+print('My name: ', name, '\nMy favorite color: ', color, '\nMy favorite activity: ', activity, '\nMy favorite animal: ', animal)
