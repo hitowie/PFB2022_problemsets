ffaedf8f8fc87fc0edc9e408feed736b24ca2350
student <student@info-25.cshl.edu>
2022-10-14 15:24:29 -0400

    added restriction enzyme script

--- python7_problemSets/restriction_enyzmes.py
+++ python7_problemSets/restriction_enyzmes.py
@@ -18,8 +18,39 @@ with open("rebase.txt","r") as enzyme_file:
 print(enzyme_dict)
 
 # take two command line arguments: enzyme and fasta file with sequence to be cut
-# if the enzyme is present in enzyme_dict AND can cut the sequence, print out:
-# sequence, annotated with cut sites
-# number of fragments
-# fragment sorted largest to smallest
+user_enzyme = sys.argv[1]
+
+# parse in fasta file
+pattern = r"^>(\w+)\s?(.*)"
+fasta_dict = {}
+
+with open(sys.argv[2], "r") as fasta_file:
+	for line in fasta_file:
+		line = line.rstrip()
+		if line.startswith(">"):
+			header = line
+			fasta_dict[header] = ''
+		else:
+			fasta_dict[header] += line
+
+# search in enzyme_dict for restriction enzyme and restriction site
+for enzymes in enzyme_dict:
+	cut_site = enzyme_dict[enzymes]
+# check if the enzyme is present in enzyme_dict
+	if user_enzyme in enzymes:
+#		print('enzyme found:', enzymes, 'cuts at', cut_site )	
+#	check if user_enzyme can cut the sequence
+		pattern_to_search_for = cut_site.replace('^','')
+		replacement = cut_site 
+		for seqID in fasta_dict:
+			seq = fasta_dict[seqID]
+			matches = re.finditer(pattern_to_search_for, seq)
+			for match in matches:
+				if match:
+					new_seq = re.sub(pattern_to_search_for, replacement, seq)
+			fragments = new_seq.split("^")
+			electrophoresis = sorted(fragments, key=len, reverse=True)
+			print(f"sequence with annotated cut sites: {new_seq}")
+			print(f"number of fragments: {len(fragments)}")
+			print(f"fragments sorted largest to smallest: {electrophoresis}")
 
