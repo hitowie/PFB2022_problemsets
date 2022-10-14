f51af24e10a317197d865992357085dcee39a19a
student <student@info-25.cshl.edu>
2022-10-14 11:44:53 -0400

    updated Python7 problem sets

--- python7_problemSets/ApoI_restriction.py
+++ python7_problemSets/ApoI_restriction.py
@@ -2,7 +2,7 @@
 import re
 
 fasta_header = r"^>(\w+)\s?(.*)" 
-ApoI_RS = r"[AG]AATT[CT]"
+ApoI_RS = r"[AG](AATT)[CT]"
 
 fasta_file = open("Python_07_ApoI.fasta", "r")
 
@@ -21,6 +21,18 @@ fasta_file.close()
 # find all ApoI restriction sites
 for seqID in seq_dict:
 	seq = seq_dict[seqID]
-	match = re.findall(ApoI_RS, seq)
-	print(match)
+	RS = re.findall(ApoI_RS, seq)
+	print(RS)
+# determine the sites of the physical cuts by ApoI in the DNA sequence and print out the sequence with ^ at the cut sites
+	new_seq = seq
+	cut_sites = re.finditer(ApoI_RS, seq)
+	for cut in cut_sites:
+		pattern = "".join(cut.group(0))
+		replacement = "".join([cut.group(1)[0], "^", cut.group(1)[1]])
+		new_seq = re.sub(pattern, replacement, new_seq)
+	print(new_seq)
 
+# determine the lengths of your fragments and sort them by length (in the same order they would separate on electrophoresis gel)
+	fragments = new_seq.split('^')
+	electrophoresis = sorted(fragments, key=len, reverse=True)
+	print(electrophoresis)
---
+++ python7_problemSets/restriction_enyzmes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import re
+import sys
+
+# fill a dictionary of enyzmes paired with their recognition patterns
+
+with open("bionet.txt","r") as enzyme_file:
+	enzyme_dict = {}
+	lines_after_header = enzyme_file.readlines()[10:]	# skip the top 10 header lines of the file
+	pattern = r"^(\S+\s\S*)\W+(\S+)"
+	for line in lines_after_header:
+		line = line.rstrip()
+		matches = re.finditer(pattern, line) # re.search().group() does not work, use finditer() and for loop to call match.group()
+		for match in matches:
+			enzyme = match.group(1)
+			recognition_site = match.group(2)
+			enzyme_dict[enzyme] = recognition_site
+print(enzyme_dict)
+
+# take two command line arguments: enzyme and fasta file with sequence to be cut
+# if the enzyme is present in enzyme_dict AND can cut the sequence, print out:
+# sequence, annotated with cut sites
+# number of fragments
+# fragment sorted largest to smallest
+
