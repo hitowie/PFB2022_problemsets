7461a361ddd1a41b17e038fe3df9bc1ff5382984
Heil <cheil@PHARMPHYS15ML.local>
2022-10-20 23:22:11 -0400

    challenge prints additional None

--- python11_problemSets/create_class.py
+++ python11_problemSets/create_class.py
@@ -31,6 +31,11 @@ class DNARecord(object):
 	def fasta_format(self):
 		print(f">{self.seq_name} {self.organism}\n{self.sequence}")
 
+# compare two DNA Sequence records method
+	def compare_to(self, other):
+		print(f"{self.seq_name} {self.organism} {self.sequence}" == f"{other.seq_name} {other.organism} {other.sequence}")
+		
+
 # write some lines of code outside your class that
 # a. uses the object sequence attribute to retrieve and print the sequence
 # b. uses the object name attribute to retrieve and print the name
@@ -45,3 +50,9 @@ print(dna_rec_obj.length())
 print(dna_rec_obj.nt_composition())
 print(dna_rec_obj.gc_content())
 dna_rec_obj.fasta_format()
+
+dna_rec_obj2 = DNARecord('ACTGATCGTTACGTACGAGT', 'ABC1', 'Drosophila melanogaster')
+dna_rec_obj3 = DNARecord('ACTGATCGTTACGTACGAGT', 'ABCD', 'Drosophila melanogaster')
+
+print(dna_rec_obj.compare_to(dna_rec_obj2))
+print(dna_rec_obj.compare_to(dna_rec_obj3))
